1648229585aa0af435b772334d3a41f2e9f08bff
Bulmang <hmk113737@gmail.com>
2022-11-25 10:53:00 +0900

    Initial Commit

---
+++ Iphone_Keypad/Iphone_KeypadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iphone_KeypadApp.swift
+//  Iphone_Keypad
+//
+//  Created by 하명관 on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Iphone_KeypadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
